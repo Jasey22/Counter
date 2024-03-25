e88f6294954161d0c01b7017286d09ae6cb1e4b6
Jasey22 <jasonmcnamara601@gmail.com>
2024-03-26 08:47:02 +1300

    intial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var counter = 0;
+
+function btn_decrease(){
+    counter--;
+    document.getElementById('count').innerHTML = counter;
+}
+
+var counter = 0;
+
+function btn_increase() {
+    counter++;
+    document.getElementById("count").innerHTML = counter;
+}
+
+var counter = 0;
+
+function btn_reset() {
+    counter = 0;
+    document.getElementById("count").innerHTML = counter;
+}
